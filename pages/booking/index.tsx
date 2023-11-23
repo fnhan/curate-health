e66282ee727c19dcf4a22ed5d022a9d79e72667a
Sean Gray <seangray.dev@gmail.com>
2023-11-22 21:01:53 -0500

    setup pages

---
+++ pages/booking/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const Booking = () => {
+  return <Layout>Booking</Layout>;
+};
+
+export default Booking;
---
+++ pages/services/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const Services = () => {
+  return <Layout>Services</Layout>;
+};
+
+export default Services;

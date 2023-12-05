7531105d2de7887e3fb24bdd69345920d0d6386c
Sean Gray <seangray.dev@gmail.com>
2023-12-05 14:20:16 -0500

    remove default bg-primary on layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ title, children }) => {
     <>
       <Meta title={title} />
       <div className='flex flex-col min-h-screen'>
-        <div className='flex-1 bg-primary text-white'>
+        <div className='flex-1 text-white'>
           <Nav />
           <main className='container'>{children}</main>
         </div>

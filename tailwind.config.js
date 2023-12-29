ec1a2947466e5f386659bf841c16797fc0a55b27
Sean Gray <seangray.dev@gmail.com>
2023-12-29 09:00:09 -0500

    update default screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1400px',
+    },
     container: {
       center: true,
       padding: '2rem',

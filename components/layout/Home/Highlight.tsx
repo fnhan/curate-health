7404606d4eded3a17b32d4963e3137c4c2044cf8
Sean Gray <seangray.dev@gmail.com>
2023-12-29 09:51:39 -0500

    add responsiveness and scroll-animation on circle image text

--- components/layout/Home/Highlight.tsx
+++ components/layout/Home/Highlight.tsx
@@ -1,30 +1,56 @@
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
+import Link from 'next/link';
 import highlightCircleText from 'public/images/CircleText.png';
 import highlightImage from 'public/images/HighlightImage.png';
 
 export default function Highlight() {
+  const { scrollYProgress } = useScroll();
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, 360]);
+
   return (
     <section className='w-full bg-primary'>
-      <div className='container py-40 flex flex-col gap-20'>
-        <div>
-          <h3 className='text-4xl max-w-[544px] font-light'>
+      <div className='container py-12 md:py-40 flex flex-col gap-20 text-left'>
+        <div className='max-w-prose md:sticky md:top-0 z-10 md:pt-10'>
+          <h3 className='max-w-[250px] md:text-2xl md:max-w-[350px] 2xl:max-w-[555px] font-light 2xl:text-[40px] 2xl:leading-10'>
             Discover a unique approach to health and wellness, tailored to meet
             your individual needs
           </h3>
         </div>
-        <div className='flex justify-center relative'>
-          <Image width={536} height={536} alt='' src={highlightImage} />
-          <div className='absolute right-60'>
-            <Image src={highlightCircleText} width={287} height={287} alt='' />
+        <div className='flex justify-center'>
+          <div className='relative inline-block'>
+            <Image
+              width={536}
+              height={536}
+              alt=''
+              src={highlightImage}
+              className='w-[200px] md:w-[310px] 2xl:w-[536px]'
+            />
+            <motion.div
+              className='absolute -top-5 -right-10 md:-right-20 2xl:-right-32'
+              style={{ rotate }}>
+              <Image
+                src={highlightCircleText}
+                width={287}
+                height={287}
+                alt=''
+                className='w-[107px] md:w-[165px] 2xl:w-[287px]'
+              />
+            </motion.div>
           </div>
         </div>
         <div className='flex justify-end'>
-          <h3 className='text-4xl max-w-[544px] font-light'>
+          <h3 className='max-w-[177px] text-sm md:text-2xl md:max-w-[350px] 2xl:max-w-[555px] font-light 2xl:text-[40px] 2xl:leading-10'>
             Explore our comprehensive services and embark on a journey to a
             healthier you.
           </h3>
         </div>
       </div>
+      <Link className='w-full' href={'/about'}>
+        <div className='container border-t duration-300 transition-all hover:bg-secondary py-5 text-right block font-denton'>
+          More About Us
+        </div>
+      </Link>
     </section>
   );
 }

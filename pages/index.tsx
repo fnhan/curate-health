7756634e5f7066ee58d1d41ff7c9dea6fc9d5508
Sean Gray <seangray.dev@gmail.com>
2023-12-30 01:02:04 -0500

    add Clinic, Services and Products components to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
+import Clinic from 'components/layout/Home/Clinic';
 import Highlight from 'components/layout/Home/Highlight';
 import HomeLayout from 'components/layout/Home/HomeLayout';
-import Intro from '../components/layout/Home/Hero';
+import Products from 'components/layout/Home/Products';
+import Services from 'components/layout/Home/Services';
 
 export default function Index() {
   return (
     <HomeLayout title={'Home'}>
       <Highlight />
+      <Clinic />
+      <Services />
+      <Products />
     </HomeLayout>
   );
 }

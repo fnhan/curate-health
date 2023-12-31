0ff352c21e690ba4a05a83f08368402d89c2a372
Sean Gray <seangray.dev@gmail.com>
2023-12-31 15:33:34 -0500

    add curate cafe section

---
+++ components/layout/Home/CurateCafe.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import CurateCafeBg from 'public/images/curate-cafe.png';
+
+export default function CurateCafe() {
+  return (
+    <section className='relative bg-white'>
+      <Image
+        src={CurateCafeBg}
+        width={1440}
+        height={970}
+        alt='Curate Cafe'
+        layout='responsive'
+        className='max-h-screen object-cover 2xl:container 2xl:px-0'
+      />
+      <div className='container absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-between py-9 md:py-14 2xl:py-18'>
+        <h2 className='text-black md:text-xl 2xl:text-3xl'>
+          Curate Health Bar
+        </h2>
+        <p className='font-denton text-black md:text-xl max-w-[200px] md:max-w-[300px] 2xl:max-w-[544px] 2xl:text-4xl italic'>
+          Discover a unique approach to health and wellness, tailored to meet
+          your individual needs.
+        </p>
+      </div>
+    </section>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,32 @@
+import Blog from 'components/layout/Home/Blog';
 import Clinic from 'components/layout/Home/Clinic';
+import CurateCafe from 'components/layout/Home/CurateCafe';
 import Highlight from 'components/layout/Home/Highlight';
 import HomeLayout from 'components/layout/Home/HomeLayout';
 import Products from 'components/layout/Home/Products';
 import Services from 'components/layout/Home/Services';
+import { getTwoMostRecentPosts } from 'lib/api';
+import { GetStaticProps } from 'next';
 
-export default function Index() {
+export default function Index({ mostRecentPosts }) {
   return (
     <HomeLayout title={'Home'}>
       <Highlight />
       <Clinic />
       <Services />
       <Products />
+      <CurateCafe />
+      <Blog posts={mostRecentPosts} />
     </HomeLayout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
+  const mostRecentPostsData = await getTwoMostRecentPosts(preview);
+  const mostRecentPosts = mostRecentPostsData.edges; // Make sure this matches the expected structure
+
+  return {
+    props: { mostRecentPosts, preview },
+    revalidate: 10,
+  };
+};

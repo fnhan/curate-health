4f22c1eab71bc41b75f5d541e3c80874f48fad8f
Sean Gray <seangray.dev@gmail.com>
2023-12-31 16:57:00 -0500

    add platinum class

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,7 @@ module.exports = {
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        platinum: 'hsl(var(--platinum))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',

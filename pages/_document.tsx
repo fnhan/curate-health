2d3064711be19de0641c2942bfc12d0c936ea435
Sean Gray <seangray.dev@gmail.com>
2024-01-03 06:52:43 -0500

    install vercel speed insights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
         <Main />
         <NextScript />
+        <SpeedInsights />
       </body>
     </Html>
-  )
+  );
 }

d936758d00f1ef13973afd054d913e71cedcb3ea
Sean Gray <seangray.dev@gmail.com>
2024-01-04 14:30:21 -0500

    update Image to take full width of screen

--- components/layout/Home/Clinic.tsx
+++ components/layout/Home/Clinic.tsx
@@ -3,7 +3,7 @@ import ClinicBG from 'public/images/clinic-bg.jpg';
 
 export default function Clinic() {
   return (
-    <section className='relative 2xl:container 2xl:p-0'>
+    <section className='relative'>
       <Image
         width={1080}
         height={1440}
@@ -12,17 +12,19 @@ export default function Clinic() {
         layout='responsive'
         className='w-full object-cover max-h-[435px] md:max-h-[649px]'
       />
-      <div className='absolute bottom-0 right-0 text-xs w-[237px] bg-white text-black flex flex-col gap-10 p-8 md:p-20 my-10 md:my-0 md:w-[496px] md:text-base'>
-        <p>
-          Focused on optimizing the 5 pillars of health - physical, mental,
-          emotional, social and spiritual, to help you achieve your highest
-          potential.
-        </p>
-        <p>
-          Our evidence-based holistic approach empowers individuals to
-          proactively manage their well-being and achieve fulfillment in all
-          areas of their lives.
-        </p>
+      <div className='relative container'>
+        <div className='absolute bottom-0 right-0 text-xs w-[237px] bg-white text-black flex flex-col gap-10 p-8 md:p-20 my-10 md:my-0 md:w-[496px] md:text-base'>
+          <p>
+            Focused on optimizing the 5 pillars of health - physical, mental,
+            emotional, social and spiritual, to help you achieve your highest
+            potential.
+          </p>
+          <p>
+            Our evidence-based holistic approach empowers individuals to
+            proactively manage their well-being and achieve fulfillment in all
+            areas of their lives.
+          </p>
+        </div>
       </div>
     </section>
   );

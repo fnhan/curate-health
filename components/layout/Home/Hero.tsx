bc8ade610a57365c3d4cbb201c143b19a4e1b161
YuqinHu <h878688736@gmail.com>
2024-01-07 16:17:51 -0800

    add book appointment button on hero section

--- components/layout/Home/Hero.tsx
+++ components/layout/Home/Hero.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link';
+
 export default function Hero() {
   return (
     <section className='flex justify-center items-center flex-col min-h-screen text-white container'>
       <h1 className='hidden md:block 2xl:text-[72px] text-[42px] text-center'>
         Health, Curated for <span className='font-bold'>you.</span>
       </h1>
+      <Link
+            className='hover:underline font-denton text-[16px] md:text-base text-center'
+            href={'/booking'}>
+            Book Appointment
+      </Link>
       <h1 className='md:hidden text-[32px]'>
         Health, <br />
         Curated for <span className='font-bold'>you.</span>

85985d68c8bd92493575b8c35cfd8ab1b2bfbf0b
Sean Gray <seangray.dev@gmail.com>
2024-01-09 15:24:55 -0500

    add CTA button in hero section

--- components/layout/Home/Hero.tsx
+++ components/layout/Home/Hero.tsx
@@ -1,8 +1,9 @@
+import { Button } from 'components/ui/button';
 import Link from 'next/link';
 
 export default function Hero() {
   return (
-    <section className='flex justify-center items-center flex-col min-h-screen text-white container'>
+    <section className='flex justify-center items-center flex-col min-h-screen text-white container gap-6'>
       <h1 className='hidden md:block 2xl:text-[72px] text-[42px] text-center'>
         Health, Curated for <span className='font-bold'>you.</span>
       </h1>
@@ -10,6 +11,11 @@ export default function Hero() {
         Health, <br />
         Curated for <span className='font-bold'>you.</span>
       </h1>
+      <Link href={'/booking'}>
+        <Button className='bg-white text-primary hover:text-white hover:bg-primary rounded-none duration-300 transition-all'>
+          Book Appointment
+        </Button>
+      </Link>
     </section>
   );
 }

c1c9ecd68aa0f0bbf78549af71d6408e4a83deb5
YuqinHu <h878688736@gmail.com>
2024-01-09 09:19:40 -0800

    delete appointment button on hero section

--- components/layout/Home/Hero.tsx
+++ components/layout/Home/Hero.tsx
@@ -6,11 +6,6 @@ export default function Hero() {
       <h1 className='hidden md:block 2xl:text-[72px] text-[42px] text-center'>
         Health, Curated for <span className='font-bold'>you.</span>
       </h1>
-      <Link
-            className='hover:underline font-denton text-[16px] md:text-base text-center'
-            href={'/booking'}>
-            Book Appointment
-      </Link>
       <h1 className='md:hidden text-[32px]'>
         Health, <br />
         Curated for <span className='font-bold'>you.</span>

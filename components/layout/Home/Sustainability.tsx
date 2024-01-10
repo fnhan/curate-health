ad74239e09a5e11cb505c4853ad8b8fd01c01430
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2024-01-10 18:00:57 -0500

    change prettier setting to use single quote

--- components/layout/Home/Sustainability.tsx
+++ components/layout/Home/Sustainability.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import SustainabilityBg from "public/images/sustainability.jpg";
+import Image from 'next/image';
+import SustainabilityBg from 'public/images/sustainability.jpg';
 
 export default function Sustainability() {
   return (
@@ -8,12 +8,12 @@ export default function Sustainability() {
         width={1080}
         height={1440}
         src={SustainabilityBg}
-        alt="clinic"
-        layout="responsive"
-        className="w-full object-cover max-h-[435px] md:max-h-[649px]"
+        alt='clinic'
+        layout='responsive'
+        className='w-full object-cover max-h-[435px] md:max-h-[649px]'
       />
-      <div className="relative 2xl:container 2xl:p-0">
-        <div className="absolute bottom-0 left-0 text-xs w-[237px] bg-white text-black flex flex-col gap-10 p-8 md:p-20 my-10 md:my-0 md:w-[496px] md:text-base">
+      <div className='relative 2xl:container 2xl:p-0'>
+        <div className='absolute bottom-0 left-0 text-xs w-[237px] bg-white text-black flex flex-col gap-10 p-8 md:p-20 my-10 md:my-0 md:w-[496px] md:text-base'>
           <p>
             Curate Health is committed to the well-being of individuals and the
             planet through environmental sustainability and social

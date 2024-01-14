d7a37839ef856dbbf430ca92a7acecbc2c4a73b5
YuqinHu <h878688736@gmail.com>
2024-01-13 17:23:36 -0800

    merge from main

--- components/layout/Home/Carousel.tsx
+++ components/layout/Home/Carousel.tsx
@@ -58,33 +58,26 @@ const SPRING_OPTIONS = {
 
 export const SwipeCarousel = () => {
   const [imgIndex, setImgIndex] = useState(0);
-
   const dragX = useMotionValue(0);
 
   useEffect(() => {
     const intervalRef = setInterval(() => {
       const x = dragX.get();
-
       if (x === 0) {
-        setImgIndex((pv) => {
-          if (pv === products.length - 1) {
-            return 0;
-          }
-          return pv + 1;
-        });
+        setImgIndex((prevIndex) => (prevIndex === products.length - 1 ? 0 : prevIndex + 1));
       }
     }, AUTO_DELAY);
 
     return () => clearInterval(intervalRef);
-  }, []);
+  }, [dragX]);
 
   const onDragEnd = () => {
     const x = dragX.get();
 
     if (x <= -DRAG_BUFFER && imgIndex < products.length - 1) {
-      setImgIndex((pv) => pv + 1);
+      setImgIndex((prevIndex) => prevIndex + 1);
     } else if (x >= DRAG_BUFFER && imgIndex > 0) {
-      setImgIndex((pv) => pv - 1);
+      setImgIndex((prevIndex) => prevIndex - 1);
     }
   };
 
@@ -107,7 +100,7 @@ export const SwipeCarousel = () => {
         className='flex cursor-grab active:cursor-grabbing'>
         <Cards imgIndex={imgIndex} />
       </motion.div>
-      <Dots imgIndex={imgIndex} setImgIndex={setImgIndex} />;
+      <Dots imgIndex={imgIndex} setImgIndex={setImgIndex} />
     </div>
   );
 };
@@ -115,51 +108,70 @@ export const SwipeCarousel = () => {
 const Cards = ({ imgIndex }) => {
   return (
     <>
-      {products.map((product, idx) => {
-        return (
-          <motion.div
-            key={idx}
-            // animate={{
-            //   scale: imgIndex === idx ? 0.95 : 0.85,
-            // }}
-            transition={SPRING_OPTIONS}
-            className='w-full shrink-0 md:w-1/2 2xl:w-1/3'>
-            <Card className='w-full bg-secondary text-white rounded-none flex flex-col h-[350px] py-5'>
-              <CardContent className='flex justify-center items-center pb-0'>
-                <Image
-                  className='mx-auto pt-6'
-                  src={product.image}
-                  width={236}
-                  height={314}
-                  alt={product.title}
-                />
-              </CardContent>
-              <CardHeader className='flex-1 pb-0 w-2/3 mx-auto'>
-                <CardTitle className='text-center font-denton mb-3'>
-                  {product.title}
-                </CardTitle>
-                <CardDescription className='text-center text-white text-xs'>
-                  {product.description}
-                </CardDescription>
-              </CardHeader>
-            </Card>
-          </motion.div>
-        );
-      })}
+      {products.map((product, idx) => (
+        <motion.div
+          key={idx}
+          transition={SPRING_OPTIONS}
+          className='w-full shrink-0 md:w-1/2 2xl:w-1/3'>
+          <Card className='w-full bg-secondary text-white rounded-none flex flex-col h-[350px] py-5'>
+            <CardContent className='flex justify-center items-center pb-0'>
+              <Image
+                className='mx-auto pt-6'
+                src={product.image}
+                width={236}
+                height={314}
+                alt={product.title}
+              />
+            </CardContent>
+            <CardHeader className='flex-1 pb-0 w-2/3 mx-auto'>
+              <CardTitle className='text-center font-denton mb-3'>
+                {product.title}
+              </CardTitle>
+              <CardDescription className='text-center text-white text-xs'>
+                {product.description}
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </motion.div>
+      ))}
     </>
   );
 };
 
 const Dots = ({ imgIndex, setImgIndex }) => {
+  const totalImages = products.length;
+  const numImagesLargeScreen = 3; // Number of images for the largest screen
+  const numImagesMediumScreen = 2; // Number of images for the second largest screen
+
+  const calculateNumDots = () => {
+    if (typeof window !== 'undefined') {
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth >= 1200) {
+        return Math.ceil(totalImages / numImagesLargeScreen);
+      } else if (screenWidth >= 768) {
+        return Math.ceil(totalImages / numImagesMediumScreen);
+      } else {
+        return totalImages;
+      }
+    }
+
+    return totalImages;
+  };
+
+  const numDots = calculateNumDots();
+
   return (
     <div className='mt-8 flex w-full justify-center gap-2'>
-      {products.map((_, idx) => {
+      {Array.from({ length: numDots }).map((_, idx) => {
+        const actualIdx = idx * numImagesLargeScreen;
+
         return (
           <button
             key={idx}
-            onClick={() => setImgIndex(idx)}
+            onClick={() => setImgIndex(actualIdx)}
             className={`h-3 w-3 rounded-full transition-colors ${
-              idx === imgIndex ? 'bg-primary' : 'bg-secondary'
+              actualIdx === imgIndex ? 'bg-primary' : 'bg-secondary'
             }`}
           />
         );

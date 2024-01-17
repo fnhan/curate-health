a70f9abf90ce2f3fa535cc0b1086f3070c0abca0
Sean Gray <seangray.dev@gmail.com>
2024-01-17 14:50:47 -0500

    add group-hover and dynamic text color

--- components/layout/Home/HoverLink.tsx
+++ components/layout/Home/HoverLink.tsx
@@ -4,15 +4,20 @@ import Link from 'next/link';
 type HoverLinkProps = {
   href: string;
   text: string;
+  textColor?: string;
 };
 
-export default function HoverLink({ href, text }: HoverLinkProps) {
+export default function HoverLink({
+  href,
+  text,
+  textColor = 'text-white',
+}: HoverLinkProps) {
   return (
     <Link className='w-full' href={href}>
-      <div className='container border-t duration-300 transition-all hover:bg-secondary py-5 text-right block font-denton-condensed italic'>
-        <div className='flex items-center gap-2 hover:gap-20 transition-all duration-300 justify-end'>
-          <span>{text}</span>
-          <ChevronRight className='w-5' />
+      <div className='container border-t duration-300 transition-all group hover:bg-secondary py-5 text-right block font-denton-condensed italic'>
+        <div className='flex font-denton items-center gap-2 hover:gap-20 transition-all duration-300 justify-end'>
+          <span className={`${textColor} group-hover:text-white`}>{text}</span>
+          <ChevronRight className={`w-5 ${textColor} group-hover:text-white`} />
         </div>
       </div>
     </Link>

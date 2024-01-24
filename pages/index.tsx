e1ba7cd4e9e4721d0951f97ca1f7999092cc3603
Sean Gray <seangray.dev@gmail.com>
2024-01-23 22:59:03 -0500

    Add Posts component and getStaticProps function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Posts from 'components/layout/Blog-Page/Posts';
 import Blog from 'components/layout/Home/Blog';
 import Clinic from 'components/layout/Home/Clinic';
 import CurateCafe from 'components/layout/Home/CurateCafe';
@@ -8,8 +9,19 @@ import Products from 'components/layout/Home/Products';
 import Services from 'components/layout/Home/Services';
 import Survey from 'components/layout/Home/Survey';
 import Sustainability from 'components/layout/Home/Sustainability';
+import { SanityDocument } from 'next-sanity';
+import dynamic from 'next/dynamic';
+import { getClient } from '../sanity/lib/client';
+import { POSTS_QUERY } from '../sanity/lib/queries';
+import { token } from '../sanity/lib/token';
 
-export default function Index() {
+type PageProps = {
+  posts: SanityDocument[];
+  draftMode: boolean;
+  token: string;
+};
+
+export default function Index({ posts, draftMode, token }: PageProps) {
   return (
     <HomeLayout title={'Home'}>
       <Highlight />
@@ -17,10 +29,23 @@ export default function Index() {
       <Services />
       <Products />
       <CurateCafe />
-      <Blog />
+      <Blog posts={posts} draftMode={draftMode} token={token} />
       <Sustainability />
       <Survey />
       <Newsletter />
     </HomeLayout>
   );
 }
+
+export const getStaticProps = async ({ draftMode = false }) => {
+  const client = getClient(draftMode ? token : undefined);
+  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY);
+
+  return {
+    props: {
+      posts,
+      draftMode,
+      token: draftMode ? token : '',
+    },
+  };
+};

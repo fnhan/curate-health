7391dd85423340ad1a7bcdbe7e58cb4127cea168
Sean Gray <seangray.dev@gmail.com>
2024-01-25 04:40:43 -0500

    Add expandable service body and read more button

--- pages/services/ServiceDetails.tsx
+++ pages/services/ServiceDetails.tsx
@@ -1,6 +1,16 @@
+import { Button } from 'components/ui/button';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function ServiceDetails({ service }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const excerptLength = 300;
+
   return (
     <section className='text-white' id={service.id}>
       <div className='container text-2xl font-denton tracking-widest flex justify-center md:justify-start mb-2'>
@@ -14,7 +24,19 @@ export default function ServiceDetails({ service }) {
         alt={service.title}
         className='w-full object-cover object-center max-h-[280px] md:max-h-[625px] mb-12'
       />
-      <p className='container'>{service.body}</p>
+      <div className='container'>
+        <p>
+          {isExpanded
+            ? service.body
+            : `${service.body.substring(0, excerptLength)}...`}
+        </p>
+        <Button
+          variant='ghost'
+          onClick={toggleExpanded}
+          className='p-0 hover:bg-transparent font-denton-condesnsed text-secondary hover:underline hover:text-white duration-300 transition-all'>
+          {isExpanded ? 'Show Less' : 'Read More'}
+        </Button>
+      </div>
     </section>
   );
 }

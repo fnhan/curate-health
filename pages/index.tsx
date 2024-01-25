9c2c1fe93770fa922e521b4995c9e0e24b737521
Sean Gray <seangray.dev@gmail.com>
2024-01-25 03:27:34 -0500

    Add Hero section to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,35 @@
-import Posts from 'components/layout/Blog-Page/Posts';
 import Blog from 'components/layout/Home/Blog';
 import Clinic from 'components/layout/Home/Clinic';
 import CurateCafe from 'components/layout/Home/CurateCafe';
+import Hero from 'components/layout/Home/Hero';
 import Highlight from 'components/layout/Home/Highlight';
-import HomeLayout from 'components/layout/Home/HomeLayout';
 import Newsletter from 'components/layout/Home/Newsletter';
 import Products from 'components/layout/Home/Products';
 import Services from 'components/layout/Home/Services';
 import Survey from 'components/layout/Home/Survey';
 import Sustainability from 'components/layout/Home/Sustainability';
+import Layout from 'components/layout/layout';
 import { SanityDocument } from 'next-sanity';
-import dynamic from 'next/dynamic';
 import { getClient } from '../sanity/lib/client';
-import { POSTS_QUERY } from '../sanity/lib/queries';
+import { POSTS_QUERY, heroSectionQuery } from '../sanity/lib/queries';
 import { token } from '../sanity/lib/token';
 
 type PageProps = {
   posts: SanityDocument[];
+  heroSection: SanityDocument[];
   draftMode: boolean;
   token: string;
 };
 
-export default function Index({ posts, draftMode, token }: PageProps) {
+export default function Index({
+  posts,
+  draftMode,
+  token,
+  heroSection,
+}: PageProps) {
   return (
-    <HomeLayout title={'Home'}>
+    <Layout title={'Home'}>
+      <Hero heroSection={heroSection} />
       <Highlight />
       <Clinic />
       <Services />
@@ -33,17 +39,19 @@ export default function Index({ posts, draftMode, token }: PageProps) {
       <Sustainability />
       <Survey />
       <Newsletter />
-    </HomeLayout>
+    </Layout>
   );
 }
 
 export const getStaticProps = async ({ draftMode = false }) => {
   const client = getClient(draftMode ? token : undefined);
   const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY);
+  const heroSection = await client.fetch<SanityDocument[]>(heroSectionQuery);
 
   return {
     props: {
       posts,
+      heroSection,
       draftMode,
       token: draftMode ? token : '',
     },

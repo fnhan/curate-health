dcda0d3a112bc2a200fd3a71a05d2de4d84dffdf
Sean Gray <seangray.dev@gmail.com>
2024-02-01 12:53:31 -0500

    Move ServicesList component to Layout/Home and adjust import paths

--- pages/services/[slug].tsx
+++ pages/services/[slug].tsx
@@ -1,5 +1,4 @@
 import ServiceDetails from 'components/layout/Services/ServiceDetails';
-import ServicesList from 'components/layout/Services/ServicesList';
 import { useRouter } from 'next/router';
 import Newsletter from '../../components/layout/Home/Newsletter';
 import Layout from '../../components/layout/layout';
@@ -13,9 +12,7 @@ const ServicePage = () => {
 
   return (
     <Layout title={service?.title || 'Services'}>
-      <div className='container pt-10'>
-        <ServicesList />
-      </div>
+      <div className='container pt-10'></div>
       <div className='py-10'>
         {service ? <ServiceDetails service={service} /> : <p>Loading...</p>}
       </div>

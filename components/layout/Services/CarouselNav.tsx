4c64eef93f57110c4109d52b46ee716220a2e782
Sean Gray <seangray.dev@gmail.com>
2024-02-01 15:34:43 -0500

    Add CarouselNav component

---
+++ components/layout/Services/CarouselNav.tsx
@@ -0,0 +1,37 @@
+import { Card, CardContent } from 'components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from 'components/ui/carousel';
+import Link from 'next/link';
+import { servicesList } from 'public/data/serviceList';
+
+export function CarouselNav() {
+  return (
+    <Carousel className='container relative'>
+      <CarouselContent className='-ml-1'>
+        {servicesList.map((service, index) => (
+          <CarouselItem
+            key={index}
+            className='pl-1 md:basis-1/2 lg:basis-1/3 border-r last:border-r-0'>
+            <div className='p-1 group'>
+              <Link href={service.href}>
+                <Card className='bg-transparent border-none'>
+                  <CardContent className='flex items-center justify-center p-6 font-denton text-white group-hover:underline'>
+                    {service.title}
+                  </CardContent>
+                </Card>
+              </Link>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+
+      <CarouselPrevious className='left-0 ml-4' />
+      <CarouselNext className='right-0 mr-4' />
+    </Carousel>
+  );
+}

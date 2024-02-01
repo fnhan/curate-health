e12f261f074b757f9154230831a665c58081a57a
Sean Gray <seangray.dev@gmail.com>
2024-02-01 15:57:08 -0500

    Update Services layout and ServicesList styling

--- components/layout/Home/Services.tsx
+++ components/layout/Home/Services.tsx
@@ -4,22 +4,9 @@ import HoverLink from './HoverLink';
 export default function Services() {
   return (
     <section>
-      <div className='container flex flex-col gap-10 md:gap-20 pt-[90px] pb-[60px]'>
-        <div className='flex flex-col md:flex-row justify-between md:gap-20'>
-          <h2 className='mb-12 text-3xl md:text-5xl'>
-            Our <br />
-            Services
-          </h2>
-          <p className='md:max-w-[736px]'>
-            Our services are designed to address the physical, mental, and
-            emotional aspects of your health, ensuring a well-rounded and
-            individualized care plan. Whether you seek routine check-ups,
-            rehabilitation, or guidance on healthy living, our clinic is
-            committed to fostering a supportive and healing environment for
-            everyone who walks through our doors. Your journey to optimal health
-            begins here, where our integrated services work together to promote
-            a healthier and happier you.
-          </p>
+      <div className='container flex flex-col gap-10 md:gap-20 pt-8 md:py-16 pb-12'>
+        <div className='flex justify-center'>
+          <h2 className='mb-6 text-3xl md:text-5xl'>Our Services</h2>
         </div>
         <ServicesList />
       </div>
--- components/layout/Services/ServicesList.tsx
+++ components/layout/Services/ServicesList.tsx
@@ -48,7 +48,7 @@ export default function ServicesList() {
   ];
 
   return (
-    <div className='md:flex justify-between grid grid-cols-3 gap-y-6 gap-x-2'>
+    <div className='md:flex justify-between grid grid-cols-2 gap-y-6 gap-x-2'>
       {servicesList.map((service) => (
         <div key={service.title}>
           <Link className='flex flex-col gap-7 group' href={service.href}>
@@ -57,7 +57,7 @@ export default function ServicesList() {
               width={175}
               height={175}
               alt=''
-              className='rounded-full w-[85px] h-[85px] 2xl:w-[175px] 2xl:h-[175px] object-cover mx-auto group-hover:-translate-y-3 transition-all duration-300'
+              className='rounded-full w-[85px] h-[85px] md:w-[128px] md:h-[128px] 2xl:w-[175px] 2xl:h-[175px] object-cover mx-auto group-hover:-translate-y-3 transition-all duration-300'
             />
             <h6 className='font-denton text-center text-md border-b border-transparent md:text-base group-hover:underline duration-300 transition-all'>
               {service.title}

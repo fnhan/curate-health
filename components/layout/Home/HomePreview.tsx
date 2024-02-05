3a2c4a1eb2f92ff94286352b490fe35edfea80b6
Sean Gray <seangray.dev@gmail.com>
2024-02-05 17:14:53 -0500

    Add HomePreview component

---
+++ components/layout/Home/HomePreview.tsx
@@ -0,0 +1,55 @@
+import { Loading } from 'components/Loading';
+import Blog from 'components/layout/Home/Blog';
+import Clinic from 'components/layout/Home/Clinic';
+import CurateCafe from 'components/layout/Home/CurateCafe';
+import Hero from 'components/layout/Home/Hero';
+import Highlight from 'components/layout/Home/Highlight';
+import Newsletter from 'components/layout/Home/Newsletter';
+import Products from 'components/layout/Home/Products';
+import Services from 'components/layout/Home/Services';
+import Survey from 'components/layout/Home/Survey';
+import Sustainability from 'components/layout/Home/Sustainability';
+import Layout from 'components/layout/layout';
+import { SanityDocument } from 'next-sanity';
+import { useLiveQuery } from 'next-sanity/preview';
+import { homePageQuery } from '../../../sanity/lib/queries';
+
+type HomePageData = {
+  heroSection: {
+    bgImage: {
+      asset: {
+        _id: string;
+        url: string;
+      };
+    };
+    heroText: any;
+  };
+  posts: SanityDocument[];
+};
+
+export default function HomePagePreview() {
+  const [data, isLoading] = useLiveQuery<HomePageData>(null, homePageQuery);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!data) {
+    return <div>Data could not be fetched.</div>;
+  }
+
+  return (
+    <Layout title='Home'>
+      <Hero heroSection={data.heroSection} />
+      <Highlight />
+      <Clinic />
+      <Services />
+      <Products />
+      <CurateCafe />
+      <Blog posts={data.posts} />
+      <Sustainability />
+      <Survey />
+      <Newsletter />
+    </Layout>
+  );
+}

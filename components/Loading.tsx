76b278b0dc9c01ea2d640dc5c4aab78ef694d65a
Sean Gray <seangray.dev@gmail.com>
2024-02-05 17:14:43 -0500

    Add Loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,10 @@
+import { Loader2 } from 'lucide-react';
+
+export function Loading() {
+  return (
+    <div className='flex flex-col gap-4 min-h-screen justify-center items-center text-white'>
+      <p>Loading</p>
+      <Loader2 className='h-10 w-10 animate-spin' />
+    </div>
+  );
+}

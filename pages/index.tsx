ed8f136543fbae7d084eb7f93911aa437e80c9aa
Sean Gray <seangray.dev@gmail.com>
2024-02-05 17:15:11 -0500

    Add dynamic import for HomePreview component

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,9 @@ import Survey from 'components/layout/Home/Survey';
 import Sustainability from 'components/layout/Home/Sustainability';
 import Layout from 'components/layout/layout';
 import { SanityDocument } from 'next-sanity';
+import dynamic from 'next/dynamic';
 import { getClient } from '../sanity/lib/client';
-import { POSTS_QUERY, heroSectionQuery } from '../sanity/lib/queries';
+import { homePageQuery } from '../sanity/lib/queries';
 import { token } from '../sanity/lib/token';
 
 type PageProps = {
@@ -21,21 +22,24 @@ type PageProps = {
   token: string;
 };
 
-export default function Index({
-  posts,
-  draftMode,
-  token,
-  heroSection,
-}: PageProps) {
+export default function Index(props: PageProps) {
+  const HomePreview = dynamic(
+    () => import('../components/layout/Home/HomePreview')
+  );
+
+  if (props.draftMode) {
+    return <HomePreview />;
+  }
+
   return (
     <Layout title={'Home'}>
-      <Hero heroSection={heroSection} />
+      <Hero heroSection={props.heroSection} />
       <Highlight />
       <Clinic />
       <Services />
       <Products />
       <CurateCafe />
-      <Blog posts={posts} draftMode={draftMode} token={token} />
+      <Blog posts={props.posts} />
       <Sustainability />
       <Survey />
       <Newsletter />
@@ -45,13 +49,11 @@ export default function Index({
 
 export const getStaticProps = async ({ draftMode = false }) => {
   const client = getClient(draftMode ? token : undefined);
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY);
-  const heroSection = await client.fetch<SanityDocument[]>(heroSectionQuery);
+  const allData = await client.fetch(homePageQuery);
 
   return {
     props: {
-      posts,
-      heroSection,
+      ...allData,
       draftMode,
       token: draftMode ? token : '',
     },

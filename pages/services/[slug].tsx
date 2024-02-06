8400484235745f8602920ca2ef708e1c97965b74
Sean Gray <seangray.dev@gmail.com>
2024-02-06 12:57:38 -0500

    Refactor ServicesPage component and add dynamic import using Sanity

--- pages/services/[slug].tsx
+++ pages/services/[slug].tsx
@@ -1,27 +1,64 @@
 import { CarouselNav } from 'components/layout/Services/CarouselNav';
 import ServiceDetails from 'components/layout/Services/ServiceDetails';
+import { GetStaticPaths } from 'next';
+import { SanityDocument } from 'next-sanity';
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import Newsletter from '../../components/layout/Home/Newsletter';
 import Layout from '../../components/layout/layout';
-import { servicesList } from '../../public/data/serviceList';
+import { getClient } from '../../sanity/lib/client';
+import { SERVICES_QUERY, SERVICES_SLUG_QUERY } from '../../sanity/lib/queries';
+import { token } from '../../sanity/lib/token';
 
-const ServicePage = () => {
+type PageProps = {
+  draftMode: boolean;
+  token: string;
+  services: SanityDocument[];
+  service: SanityDocument[];
+};
+
+export default function ServicesPage(props: PageProps) {
+  const ServicesPreview = dynamic(
+    () => import('../../components/layout/Services/ServicesPreview')
+  );
+
+  if (props.draftMode) {
+    return <ServicesPreview />;
+  }
+
   const router = useRouter();
   const { slug } = router.query;
 
-  const service = servicesList.find((service) => service.id === slug);
+  const service = props.services.find((service) => service.slug === slug);
 
   return (
     <Layout title={service?.title || 'Services'}>
       <div className='bg-secondary/60 backdrop-blur-3xl sticky top-[105px] z-50'>
-        <CarouselNav />
+        <CarouselNav services={props.services} />
       </div>
       <div className='py-10'>
-        {service ? <ServiceDetails service={service} /> : <p>Loading...</p>}
+        <ServiceDetails service={props.service} />
       </div>
       <Newsletter />
     </Layout>
   );
+}
+
+export const getStaticProps = async ({ draftMode = false }) => {
+  const client = getClient(draftMode ? token : undefined);
+  const services = await client.fetch(SERVICES_QUERY);
+
+  return {
+    props: {
+      services,
+      draftMode,
+      token: draftMode ? token : '',
+    },
+  };
 };
 
-export default ServicePage;
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getClient().fetch(SERVICES_SLUG_QUERY);
+
+  return { paths, fallback: true };
+};

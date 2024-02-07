e298c3e430c1aec51e35f0db5b0d7063963c0555
Sean Gray <seangray.dev@gmail.com>
2024-02-06 21:28:58 -0500

    Refactor Products component layout

--- components/layout/Home/Products.tsx
+++ components/layout/Home/Products.tsx
@@ -7,7 +7,9 @@ export default function Products() {
       <div className='container pt-9'>
         <h2 className=' text-black md:text-xl'>Products</h2>
       </div>
-      <ProductCarousel />
+      <div className='container'>
+        <ProductCarousel />
+      </div>
       <HoverLink
         href='/products'
         text='View All Products'

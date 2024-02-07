6ac655844491ba03a13c8fe5ec2a5213387d7773
Sean Gray <seangray.dev@gmail.com>
2024-02-07 00:36:07 -0500

    Remove unnecessary CSS class from Loading component

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from 'lucide-react';
 
 export function Loading() {
   return (
-    <div className='flex flex-col gap-4 min-h-screen justify-center items-center text-white'>
+    <div className='flex flex-col gap-4 justify-center items-center text-white'>
       <p>Loading</p>
       <Loader2 className='h-10 w-10 animate-spin' />
     </div>

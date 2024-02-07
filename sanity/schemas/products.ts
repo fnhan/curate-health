8114bf3613df79514b78e61a930c6a059976184d
Sean Gray <seangray.dev@gmail.com>
2024-02-07 00:35:14 -0500

    Add product and productsSection schemas

---
+++ sanity/schemas/products.ts
@@ -0,0 +1,62 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'product',
+  title: 'Products',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required().error('A title is required'),
+    }),
+    defineField({
+      name: 'description',
+      title: 'Product Description',
+      type: 'string',
+      validation: (Rule) => Rule.required().error('A description is required'),
+    }),
+    defineField({
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      validation: (Rule) => Rule.required().error('An image is required'),
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+          description:
+            'Describes the appearance and function of the image. Important for SEO and accessibility. Should be concise and informative.',
+        },
+      ],
+    }),
+    defineField({
+      name: 'isActive',
+      title: 'Is Active',
+      type: 'boolean',
+      description: 'Toggle to show/hide this service on the website.',
+      initialValue: true,
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      isActive: 'isActive',
+      media: 'image',
+    },
+    prepare(selection) {
+      const { title, isActive, media } = selection;
+      return {
+        title: title,
+        subtitle: `Status: ${isActive ? 'Active' : 'Inactive'}`,
+        media: media,
+      };
+    },
+  },
+});

05f723236c980ec8f6a6880c9f1f131a855bdf8b
Sean Gray <seangray.dev@gmail.com>
2024-02-08 12:33:39 -0500

    Refactor CurateCafe component and add hover link

--- components/layout/Home/CurateCafe.tsx
+++ components/layout/Home/CurateCafe.tsx
@@ -1,13 +1,15 @@
-import HoverLink from './HoverLink';
 import { PortableText } from '@portabletext/react';
 import imageUrlBuilder from '@sanity/image-url';
 import Image from 'next/image';
 import { dataset, projectId } from '../../../sanity/env';
+import HoverLink from './HoverLink';
 
 const builder = imageUrlBuilder({ projectId, dataset });
 
-export default function CurateCafe({cafeSection}) {
-  const { cafeImage, title, content } = cafeSection;
+export default function CurateCafe({ cafeSection }) {
+  const { cafeImage, title, content, hoverLinkText, hoverLinkHref } =
+    cafeSection;
+
   return (
     <>
       <section className='relative bg-white'>
@@ -20,22 +22,21 @@ export default function CurateCafe({cafeSection}) {
             .height(970)
             .quality(80)
             .url()}
-
           alt='Curate Cafe'
           className='max-h-[1000px] object-cover w-full'
         />
         <div className='container absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-between pt-9 pb-24 md:pt-14 2xl:pt-18'>
           <h2 className='text-black md:text-xl 2xl:text-3xl'>
-          <PortableText value={title} />
+            <PortableText value={title} />
           </h2>
-          <p className='font-denton text-black md:text-xl max-w-[200px] md:max-w-[300px] 2xl:max-w-[544px] 2xl:text-4xl italic'>
-          <PortableText value={content} />
-          </p>
+          <div className='font-denton text-black md:text-xl max-w-[200px] md:max-w-[300px] 2xl:max-w-[544px] 2xl:text-4xl italic'>
+            <PortableText value={content} />
+          </div>
         </div>
         <div className='absolute bottom-0 left-0 right-0 z-10'>
           <HoverLink
-            href='/cafe'
-            text='Visit the cafe'
+            href={hoverLinkHref}
+            text={hoverLinkText}
             textColor='text-black'
           />
         </div>

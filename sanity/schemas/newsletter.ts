dc15073277c7f1d6eb453a9b2947ac78226949a9
Sean Gray <seangray.dev@gmail.com>
2024-02-13 18:40:27 -0500

    Add newsletter section schema

---
+++ sanity/schemas/newsletter.ts
@@ -0,0 +1,24 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'newsletterSection',
+  title: 'Newsletter',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'endpointUrl',
+      title: 'Endpoint URL',
+      type: 'string',
+      description:
+        'Enter the URL of your Systeme.io API endpoint. You can find this in your Systeme.io dashboard.',
+    }),
+  ],
+
+  preview: {
+    prepare() {
+      return {
+        title: 'Newsletter Settings',
+      };
+    },
+  },
+});

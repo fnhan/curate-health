e732bf904fff254a5116875e728d67523645c636
Sean Gray <seangray.dev@gmail.com>
2024-02-13 09:35:55 -0500

    Refactor Hero component to use a video background

--- components/layout/Home/Hero.tsx
+++ components/layout/Home/Hero.tsx
@@ -2,16 +2,25 @@ import { PortableText } from '@portabletext/react';
 import BookAppointment from '../Buttons/BookAppointment';
 
 export default function Hero({ heroSection }) {
-  const bgImageUrl = heroSection.bgImage?.asset?.url;
+  const { videoID, heroText } = heroSection;
+
+  const videoSrc = `https://player.vimeo.com/video/${videoID}?muted=1&autoplay=1&autopause=0&pip=0&controls=0&loop=1&background=1&quality=undefined&app_id=58479&texttrack=undefined`;
 
   return (
-    <section
-      style={{ backgroundImage: `url(${bgImageUrl})` }}
-      className='bg-cover bg-fixed flex justify-center items-center flex-col min-h-screen text-white'>
-      <h1 className='text-[32px] md:text-[42px] 2xl:text-[72px] text-center mb-6'>
-        <PortableText value={heroSection.heroText} />
-      </h1>
-      <BookAppointment />
+    <section className='relative flex justify-center items-center flex-col text-white overflow-hidden h-[600px] 2xl:h-[1080px]'>
+      <div className='absolute top-0 left-0 w-full h-full overflow-hidden -mt-4'>
+        <iframe
+          src={videoSrc}
+          allow='autoplay; fullscreen; picture-in-picture'
+          className='absolute top-1/2 left-1/2 w-full h-[56.25vw] min-h-full min-w-[177.77vh] -translate-x-1/2 -translate-y-1/2 z-[-10]'
+          title='curate-health-home-video'></iframe>
+      </div>
+      <div className='z-10 text-center'>
+        <h1 className='text-[32px] md:text-[42px] 2xl:text-[72px] mb-6'>
+          <PortableText value={heroText} />
+        </h1>
+        <BookAppointment />
+      </div>
     </section>
   );
 }

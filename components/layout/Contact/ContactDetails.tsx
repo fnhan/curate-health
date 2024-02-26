b86b7d0402f1916793e0ab737f36637416770812
Britt4444 <brittferren@gmail.com>
2024-02-26 13:39:06 -0600

    build sections of contactUs page

---
+++ components/layout/Contact/ContactDetails.tsx
@@ -0,0 +1,13 @@
+
+export default function ContactDetails({ }) {
+  return (
+    <section className='bg-white flex flex-col gap-10'>
+      <div className='container flex flex-col gap-2 md:gap-4 pt-8 md:py-16 pb-12 text-black'>
+        <h5 className='md:text-xl 2xl:text-3xl'>Curate Health</h5>
+        <p>
+          989 Eglinton Ave W, York, ON, M6C 2C6<br /> (Location at the West Corner Suite)
+        </p>
+      </div>
+    </section>
+  )
+};
--- components/layout/Contact/ContactUs.tsx
+++ components/layout/Contact/ContactUs.tsx
@@ -1,36 +1,43 @@
 import Image from 'next/image';
+import ContactDetails from './ContactDetails';
+import Newsletter from '../Home/Newsletter';
 
 export default function ContactUs({ }) {
   return (
-    <section className='relative bg-platinum h-[435px] md:h-[1136px]'>
-      <Image
-        src="/images/contact-leaves.jpg"
-        // src={builder
-        //   .image(bgImage)
-        //   .quality(80)
-        //   .size(1440, 1080)
-        //   .auto('format')
-        //   .url()}
-        width={1438}
-        height={1136}
-        alt="alt"
-        className='w-full h-full object-fit'
-      />
-      <div className='absolute inset-0 flex items-center justify-center pt-12'>
-        <div className='flex justify-center items-center bg-primary h-[400px] md:h-[600px] md:w-[450px] font-denton font-bold text-xl'>
-          <h1>Contact Us</h1>
+    <>
+      <section className='relative bg-white h-[435px] md:h-[1136px]'>
+        <Image
+          src="/images/contact-leaves.jpg"
+          // src={builder
+          //   .image(bgImage)
+          //   .quality(80)
+          //   .size(1440, 1080)
+          //   .auto('format')
+          //   .url()}
+          width={1438}
+          height={1136}
+          alt="alt"
+          className='w-full h-full object-fit'
+        />
+        <div className='absolute inset-0 flex items-center justify-center pt-12'>
+          <div className='container flex flex-col gap-2 md:gap-4 pt-8 md:py-16 pb-12'>
+            <div className='flex justify-start'>
+              <h1 className='text-[32px] md:text-[42px] 2xl:text-[72px] mb-6'>Contact Us</h1>
+            </div>
+            <h2>Location</h2>
+            <p className='md:text-xl 2xl:text-3xl'>
+              West Corner Suite, 989 Eglinton Ave W, <br />
+              York, ON, M6C 2C6
+            </p>
+            <h3>Email</h3>
+            <p className='md:text-xl 2xl:text-3xl'>hello@curatehealth.ca</p>
+            <h4>Phone</h4>
+            <p className='md:text-xl 2xl:text-3xl'>(728)-682-2618</p>
+          </div>
         </div>
-        <iframe
-          id="JotFormIFrame-240504327725250"
-          title="Curate Health Contact Us Form"
-          allow="geolocation; microphone; camera"
-          src="https://form.jotform.com/240504327725250"
-          allowFullScreen
-          width="70%"
-          height="100%"
-        >
-        </iframe>
-      </div>
-    </section>
+      </section>
+      <ContactDetails></ContactDetails>
+      <Newsletter></Newsletter>
+    </>
   )
 }

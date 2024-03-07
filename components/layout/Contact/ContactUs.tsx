7a0f77ad2a21ee0edbfacb8ea9146eb6f32ee501
Britt4444 <brittferren@gmail.com>
2024-03-07 13:56:17 -0600

    resize GetDirections btn, fix hours container sizing, fix icon/font sizing

--- components/layout/Contact/ContactUs.tsx
+++ components/layout/Contact/ContactUs.tsx
@@ -24,38 +24,38 @@ export default function ContactUs({ }) {
         <div className='absolute inset-0 flex items-center justify-center pt-12'>
           <div className='container flex flex-col gap-2 md:gap-4 pt-8 md:py-16 pb-12'>
             <div className='flex justify-start'>
-              <h1 className='text-[28px] md:text-[50px] 2xl:text-[72px] mb-6'>Contact Info</h1>
+              <h1 className='text-[28px] md:text-[50px] lg:text-[72px] mb-6'>Contact Info</h1>
             </div>
             <address>
-              <div className='flex gap-3 -mb-3'>
-                <MapPin className="w-4.5 h-4.5 md:w-6 md:h-6" />
-                <h2 className='text-2.5 md:text-4 not-italic'>Location</h2>
+              <div className='flex gap-3'>
+                <MapPin className="w-[18px] h-[18px] md:w-[24px] md:h-[24px]" />
+                <h2 className='text-[10px] md:text-[14px] lg:text-[18px] not-italic pt-0.5 lg:pt-0 lg:-mt-0.5'>Location</h2>
               </div>
-              <div className='indent-9 -mb-3'>
-                <a className='text-[14px] md:text-[28px] 2xl:text-[32px] not-italic' target='_blank' href={'https://maps.app.goo.gl/o3Ff3JmxNKssVWRAA'}>
+              <div className='indent-8 md:indent-9 -mb-2'>
+                <a className='text-[14px] md:text-[28px] lg:text-[32px] not-italic' target='_blank' href={'https://maps.app.goo.gl/o3Ff3JmxNKssVWRAA'}>
                   West Corner Suite, 989 Eglinton Ave W,
                 </a>
               </div>
-              <div className='indent-9 pb-4'>
-                <a className='text-[14px] md:text-[28px] 2xl:text-[32px] not-italic' target='_blank' href={'https://maps.app.goo.gl/o3Ff3JmxNKssVWRAA'}>
+              <div className='indent-8 md:indent-9 pb-4'>
+                <a className='text-[14px] md:text-[28px] lg:text-[32px] not-italic' target='_blank' href={'https://maps.app.goo.gl/o3Ff3JmxNKssVWRAA'}>
                   York, ON, M6C 2C6
                 </a>
               </div>
-              <div className='flex gap-3 -mb-3'>
-                <Mail className="w-4.5 h-4.5 md:w-6 md:h-6" />
-                <h3 className='text-2.5 md:text-4 not-italic'>Email</h3>
+              <div className='flex gap-3'>
+                <Mail className="w-[18px] h-[18px] md:w-[24px] md:h-[24px]" />
+                <h3 className='text-[10px] md:text-[14px] lg:text-[18px] not-italic pt-0.5 lg:pt-0 lg:-mt-0.5'>Email</h3>
               </div>
-              <div className='indent-9 pb-4'>
-                <a className='text-[14px] md:text-[28px] 2xl:text-[32px] not-italic' target='_blank' href="mailto:hello@curatehealth.ca">
+              <div className='indent-8 md:indent-9 pb-4'>
+                <a className='text-[14px] md:text-[28px] lg:text-[32px] not-italic' target='_blank' href="mailto:hello@curatehealth.ca">
                   hello@curatehealth.ca
                 </a>
               </div>
-              <div className='flex gap-3 -mb-3'>
-                <Phone className="w-4.5 h-4.5 md:w-6 md:h-6" />
-                <h4 className='text-2.5 md:text-4 not-italic'>Phone</h4>
+              <div className='flex gap-3'>
+                <Phone className="w-[18px] h-[18px] md:w-[24px] md:h-[24px]" />
+                <h4 className='text-[10px] md:text-[14px] lg:text-[18px] not-italic pt-0.5 lg:pt-0 lg:-mt-0.5'>Phone</h4>
               </div>
-              <div className='indent-9 pb-4'>
-                <a className='text-[14px] md:text-[28px] 2xl:text-[32px] not-italic' target='_blank' href="tel:+17286822618">
+              <div className='indent-8 md:indent-9 pb-4'>
+                <a className='text-[14px] md:text-[28px] lg:text-[32px] not-italic' target='_blank' href="tel:+17286822618">
                   (728)-682-2618
                 </a>
               </div>

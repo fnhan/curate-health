11717054b50b0a01ac70cd88a8e0a7c497f3d33e
Britt4444 <brittferren@gmail.com>
2024-03-12 11:00:59 -0500

    minor layout/content edits

--- components/layout/Contact/ContactDetails.tsx
+++ components/layout/Contact/ContactDetails.tsx
@@ -5,12 +5,10 @@ export default function ContactDetails({ }) {
   return (
     <section className='bg-white flex flex-col py-10 md:py-20'>
       <div className="flex flex-col justify-center lg:grid lg:grid-flow-col lg:grid-row-gap-2">
-        <div className='container flex flex-col pt-4 lg:pt-12 text-primary lg:col-start-2 lg:row-span-full'>
-          <span className='text-[12px] md:text-[16px] lg:text-[24px]'>
-            989 Eglinton Ave W, York, ON, M6C 2C6<br /> (Located at the West Corner Suite)
-          </span>
+        <div className='container flex flex-col lg:pt-20 text-primary lg:col-start-2 lg:row-span-full'>
+          <h2 className='text-[18px] md:text-[28px] lg:text-[40px]'>Opening Hours</h2>
         </div>
-        <div className="container flex flex-col lg:col-start-2 lg:row-span-full pt-10 lg:pt-40 lg:mt-16">
+        <div className="container flex flex-col lg:col-start-2 lg:row-span-full pt-4 md:pt-8 lg:pt-40 lg:mt-4">
           <div className="grid grid-cols-2 grid-rows-7 text-primary md:h-[244px] w-[160px] md:w-[240px] lg:w-[326px] pb-4 text-[10px] md:text-[12px] lg:text-[16px]">
             <div className="font-bold">Monday</div>
             <div>9:00 AM - 7:00 PM</div>
@@ -30,7 +28,7 @@ export default function ContactDetails({ }) {
           <GetDirections />
         </div>
         <div className="lg:row-span-2 py-8 lg:pl-16 lg:col-start-3">
-        <DirectionsMap/>
+          <DirectionsMap />
         </div>
       </div>
     </section>

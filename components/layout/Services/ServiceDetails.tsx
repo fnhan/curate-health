cce471ad187f682dc96f701bb2753195ed1f378c
Ian hu <h878688736@gmail.com>
2024-05-09 16:15:31 -0700

    update

--- components/layout/Services/ServiceDetails.tsx
+++ components/layout/Services/ServiceDetails.tsx
@@ -12,20 +12,16 @@ export default function ServiceDetails({ service }) {
   }
 
   return (
-    <section className='relative text-black bg-white md:h-[760px]' id={service.id}>
+    <section className='relative text-black bg-white md:h-[560px] 2xl:h-[860px]' id={service.id}>
       <div className='w-full flex flex-col md:flex-row'>
-        <div className='mr-10 md:w-1/2 ml-20 flex flex-col justify-center'>
-          <h2 className='text-stone-800 text-[35px] font-medium font-[Poppins] text-2xl tracking-widest'>{service.title}</h2>
+        <div className='mt-20 mr-10 md:w-1/2 md:mt-20 ml-20 flex flex-col justify-center'>
+          <h2 className='text-stone-800 text-[20px] md:text-[40px] 2xl:text-[60px] font-[Poppins] tracking-widest'>{service.title}</h2>
           <br/>
-          <div className='text-base font-normal font-Poppins leading-[30px] tracking-tight'>
+          <div className='text-[11px] mb-10 md:text-[16px] md:leading-[30px] font-normal font-Poppins leading-[20px] md:mb-20 tracking-tight'>
             <PortableText value={service.content} />
           </div>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <div className="text-stone-800 text-xl font-light italic font-['Poppins'] leading-loose">Exercise Therapy</div>
-          <div className="text-stone-800 text-xl font-light italic font-['Poppins'] leading-loose">Nutritional Counseling</div>
+          <div className="text-stone-800 text-[12px] md:text-[18px] 2xl:text-[24px] font-light italic font-['Poppins'] leading-[10px]">Exercise Therapy</div>
+          <div className="text-stone-800 text-[12px] md:text-[18px] 2xl:text-[24px] font-light italic font-['Poppins'] leading-[80px]">Nutritional Counseling</div>
         </div>
         <div className='md:w-1/2 flex justify-end'>
           <Image
@@ -39,7 +35,7 @@ export default function ServiceDetails({ service }) {
               .quality(80)
               .url()}
             alt={service.title}
-            className='rounded-full w-[300px] h-[300px] md:mt-20 md:w-[300px] md:h-[300px] 2xl:w-[500px] 2xl:h-[500px] object-cover mx-auto group-hover:-translate-y-3 transition-all duration-300'/>
+            className='rounded-full w-[0px] h-[0px] mt-20 md:w-[280px] md:h-[280px] 2xl:w-[500px] 2xl:h-[500px] object-cover mx-auto group-hover:-translate-y-3 '/>
         </div>
       </div>
     </section>

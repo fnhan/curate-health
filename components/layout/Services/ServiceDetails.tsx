60890f38d4688d0914243f32c2c4bbcb14581a25
Ian hu <h878688736@gmail.com>
2024-05-13 01:14:10 -0700

    update moblie on service page

--- components/layout/Services/ServiceDetails.tsx
+++ components/layout/Services/ServiceDetails.tsx
@@ -12,32 +12,34 @@ export default function ServiceDetails({ service }) {
   }
 
   return (
-    <section className='relative text-black bg-white md:h-[560px] 2xl:h-[860px]' id={service.id}>
-      <div className='w-full flex flex-col md:flex-row'>
-        <div className='mt-20 mr-10 md:w-1/2 md:mt-20 ml-20 flex flex-col justify-center'>
-          <h2 className='text-stone-800 text-[20px] md:text-[40px] 2xl:text-[60px] font-[Poppins] tracking-widest'>{service.title}</h2>
-          <br/>
-          <div className='text-[11px] mb-10 md:text-[16px] md:leading-[30px] font-normal font-Poppins leading-[20px] md:mb-20 tracking-tight'>
-            <PortableText value={service.content} />
-          </div>
-          <div className="text-stone-800 text-[12px] md:text-[18px] 2xl:text-[24px] font-light italic font-['Poppins'] leading-[10px]">Exercise Therapy</div>
-          <div className="text-stone-800 text-[12px] md:text-[18px] 2xl:text-[24px] font-light italic font-['Poppins'] leading-[80px]">Nutritional Counseling</div>
-        </div>
-        <div className='md:w-1/2 flex justify-end'>
-          <Image
-            loading='lazy'
-            width={1080}
-            height={1440}
-            src={builder
-              .image(service.image)
-              .width(1080)
-              .height(1440)
-              .quality(80)
-              .url()}
-            alt={service.title}
-            className='rounded-full w-[0px] h-[0px] mt-20 md:w-[280px] md:h-[280px] 2xl:w-[500px] 2xl:h-[500px] object-cover mx-auto group-hover:-translate-y-3 '/>
-        </div>
+<section className='relative text-black bg-white md:h-[560px] 2xl:h-[860px]' id={service.id}>
+  <div className='w-full flex flex-col md:flex-row'>
+    <div className='mt-12 md:mt-28 mr-10 md:w-1/2  ml-20 flex flex-col'>
+      <h2 className='text-stone-800 text-[24px] md:text-[35px] 2xl:text-[60px] font-[Poppins] tracking-widest whitespace-nowrap'>{service.title}</h2>
+      <br/>
+      <div className='text-[12px] mb-10 md:text-[16px] md:leading-[30px] font-normal font-Poppins leading-[20px] tracking-tight'>
+        <PortableText value={service.content} />
       </div>
-    </section>
+    </div>
+    <div className='md:w-1/2 flex'>
+      <Image
+        loading='lazy'
+        width={1080}
+        height={1440}
+        src={builder
+          .image(service.image)
+          .width(1080)
+          .height(1440)
+          .quality(80)
+          .url()}
+        alt={service.title}
+        className='rounded-full w-[250px] h-[250px] md:mt-20 md:w-[350px] md:h-[350px] 2xl:w-[500px] 2xl:h-[500px] object-cover mx-auto group-hover:-translate-y-3 '/>
+    </div>
+  </div>
+  <div className="w-1/2 pt-20 pb-20 md:-mt-36 2xl:-mt-60 ml-20 text-stone-800 text-[16px] md:text-[18px] 2xl:text-[24px] font-light italic font-['Poppins'] leading-[30px] md:leading-[60px]">
+      <div>Exercise Therapy</div>
+      <div>Nutritional Counseling</div>
+  </div>
+</section>
   );
 }

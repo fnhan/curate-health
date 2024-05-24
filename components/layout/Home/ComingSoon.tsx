53d26d178c37b8aa375855087b39e516a55e4c65
Sean Gray <seangray.dev@gmail.com>
2024-05-24 12:25:20 -0400

    modify coming soon preview

--- components/layout/Home/ComingSoon.tsx
+++ components/layout/Home/ComingSoon.tsx
@@ -5,8 +5,8 @@ import logo from '../../../public/images/logo-large.png';
 export default function ComingSoon() {
   return (
     <div className='flex flex-col gap-16 justify-center items-center'>
-      <h1 className='text-4xl md:text:5xl lg:text-6xl'>Curate Health</h1>
       <Image src={logo} width={200} height={200} alt='logo' />
+      <h1 className='text-4xl md:text:5xl lg:text-6xl'>Curate Health</h1>
       <h2 className='text-2xl md:text:3xl lg:text-4xl'>Coming Soon...</h2>
     </div>
   );

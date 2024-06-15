866cd808f0e9911496f2c22e09740f40d9bdb7d2
Ian hu <h878688736@gmail.com>
2024-06-15 16:30:06 -0700

    update hover effect on service section

--- components/layout/Home/Services.tsx
+++ components/layout/Home/Services.tsx
@@ -4,7 +4,7 @@ import ServicesList from './ServicesList';
 export default function Services({ services }) {
   return (
     <section>
-      <div className='flex flex-col justify-start gap-10 md:gap-20 pt-8 md:py-16 pb-12 md:container'>
+      <div className='flex flex-col justify-start gap-10 md:gap-20 pt-8 md:py-16 pb-12 '>
         <div className='flex justify-start container'>
           <h2 className='mt-10 text-3xl md:text-5xl'>Our Services</h2>
         </div>

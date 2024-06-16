dc46383784e8aee0305c02af720f3b72764f8eb6
Ian hu <h878688736@gmail.com>
2024-06-16 11:42:33 -0700

    update service page

--- components/layout/Services/CarouselNav.tsx
+++ components/layout/Services/CarouselNav.tsx
@@ -1,25 +1,18 @@
-// import { Card, CardContent } from 'components/ui/card';
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from 'components/ui/carousel';
+
 import Link from 'next/link';
 import styles from '../../../styles/CarouselNav.module.css'
 
 export function CarouselNav({ services, currentPageTitle }) {
   return (
-    <div className={`container relative whitespace-nowrap overflow-x-auto ${styles.customScrollbar}`}>
+    <div className={`container whitespace-nowrap overflow-x-auto ${styles.customScrollbar}`}>
       <div className='flex'>
-        <div className=' md:mr-20'>
+        <div className=' -mr-8'>
           <div className='p-1 group'>
             <Link href='/services/our-services'>
               <div className='bg-transparent border-none'>
-                <div className={`items-center justify-center p-6 text-black text-[12px] 2xl:text-[14px] group-hover:underline ${currentPageTitle === "Our Services" ? 'underline text-black' : ''}`}>
+                <div className={`p-6 text-black font-light font-Poppins text-[12px] 2xl:text-[14px] group-hover:underline ${currentPageTitle === "Our Services" ? 'underline text-black' : ''}`}>
                   Our Services
-                  <span className='-mr-6 md:ml-20 mx-3 lg:inline'>|</span>
+                  <span className='ml-8 mx-3 lg:inline'>|</span>
                 </div>
               </div>
             </Link>
@@ -32,7 +25,7 @@ export function CarouselNav({ services, currentPageTitle }) {
             <div className='p-1 group'>
               <Link href={`/services/${service.slug}`}>
                 <div className={`bg-transparent border-none ${currentPageTitle === service.title ? 'underline text-black' : ''}`}>
-                  <div className='items-center justify-center p-6 text-black text-[12px] 2xl:text-[14px] hover:underline'>
+                  <div className='items-center font-light font-Poppins justify-center p-6 text-black text-[12px] 2xl:text-[14px] hover:underline'>
                     {service.title}
                   </div>
                 </div>
--- components/layout/Services/ServiceDetails.tsx
+++ components/layout/Services/ServiceDetails.tsx
@@ -14,12 +14,12 @@ export default function ServiceDetails({ service }) {
   }
 
   return (
-<section className='relative text-black bg-white md:h-[560px] 2xl:h-[860px]' id={service.id}>
+<section className='relative text-black bg-white md:h-[560px] 2xl:h-[660px]' id={service.id}>
   <div className='w-full flex flex-col md:flex-row'>
-    <div className='mt-12 md:mt-28 mr-10 md:w-1/2  ml-20 flex flex-col'>
+    <div className='mt-12 md:mt-32 mr-10 md:w-1/2  ml-20 flex flex-col'>
       <h2 className='text-stone-800 text-[24px] md:text-[35px] 2xl:text-[60px] font-[Poppins] tracking-widest whitespace-nowrap'>{service.title}</h2>
       <br/>
-      <div className='text-[12px] mb-10 md:text-[16px] md:leading-[30px] font-normal font-Poppins leading-[20px] tracking-tight'>
+      <div className='text-[12px] mb-10 md:text-[16px] md:leading-[30px] font-light font-Poppins leading-[20px] tracking-tight'>
         <PortableText value={service.content} />
       </div>
     </div>
@@ -38,7 +38,7 @@ export default function ServiceDetails({ service }) {
         className='rounded-full w-[250px] h-[250px] md:mt-20 md:w-[350px] md:h-[350px] 2xl:w-[500px] 2xl:h-[500px] object-cover mx-auto group-hover:-translate-y-3 '/>
     </div>
   </div>
-  <div className="w-1/2 pt-20 pb-20 md:-mt-36 2xl:-mt-60 ml-20 text-stone-800 text-[16px] md:text-[18px] 2xl:text-[24px] font-light italic font-['Poppins'] leading-[30px] md:leading-[60px]">
+  <div className="w-1/2 pt-10 pb-20 md:-mt-36 2xl:-mt-60 ml-20 text-stone-800 text-[16px] md:text-[18px] 2xl:text-[24px] font-light italic font-['Poppins'] leading-[30px] md:leading-[60px]">
     <div className="flex items-center"> 
         <div>Exercise Therapy</div>
         <Button 

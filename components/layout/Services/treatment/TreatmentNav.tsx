e52838ba21246fd8e2075a4e4c48bce49e996074
Ian hu <h878688736@gmail.com>
2024-07-16 21:17:16 -0700

    our service sanity

--- components/layout/Services/treatment/TreatmentNav.tsx
+++ components/layout/Services/treatment/TreatmentNav.tsx
@@ -14,7 +14,7 @@ export function TreatmentNav({ treatments, currentPageTitle, serviceTitle, servi
                   <div
                     className={`p-6 text-black font-light font-Poppins text-[12px] 2xl:text-[14px]`}>
                     {serviceTitle}
-                    <div className="-mt-1 bg-black h-[1.35px] w-0 group-hover:w-full transition-all duration-500"></div>
+                    <div className="-mt-1 bg-black h-[0.5px] md:h-[1.35px] w-0 group-hover:w-full transition-all duration-500"></div>
                   </div>
                   <div className='text-black font-light -ml-6 p-6 mx-3 lg:inline'>
                     |
@@ -33,7 +33,7 @@ export function TreatmentNav({ treatments, currentPageTitle, serviceTitle, servi
                     className={`bg-transparent border-none ${currentPageTitle === treatment.title ? 'underline text-black' : ''}`}>
                     <div className='-ml-4 -mr-4 items-center font-light font-Poppins justify-center p-6 text-black text-[12px] 2xl:text-[14px]'>
                       {treatment.title}
-                      <div className={`${currentPageTitle !== treatment.title ? '-mt-1 bg-black h-[1.35px] w-0 group-hover:w-full transition-all duration-500' : ''}`}></div>
+                      <div className={`${currentPageTitle !== treatment.title ? '-mt-1 bg-black h-[0.5px] md:h-[1.35px] w-0 group-hover:w-full transition-all duration-500' : ''}`}></div>
                     </div>
                   </div>
                 </Link>

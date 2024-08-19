daa742485e116a628979f8e0994fcc5bd236c41c
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2024-08-19 15:45:02 -0400

    Add bullet point, fix image styling

--- components/layout/Product.tsx
+++ components/layout/Product.tsx
@@ -1,4 +1,4 @@
-import { PortableText } from '@portabletext/react';
+import { PortableText, PortableTextReactComponents } from '@portabletext/react';
 import imageUrlBuilder from '@sanity/image-url';
 import { SanityDocument } from 'next-sanity';
 import Image from 'next/image';
@@ -13,17 +13,34 @@ import { Console } from 'console';
 
 const builder = imageUrlBuilder({ projectId, dataset });
 
+const components: Partial<PortableTextReactComponents> = {
+  list: {
+    // Ex. 1: customizing common list types
+    bullet: ({ children }) => (
+      <ul className='list-disc list-inside mt-xl'>{children}</ul>
+    ),
+    number: ({ children }) => (
+      <ol className='list-decimal list-inside mt-lg'>{children}</ol>
+    ),
+
+    // Ex. 2: rendering custom lists
+    checkmarks: ({ children }) => (
+      <ol className='m-auto text-lg'>{children}</ol>
+    ),
+  },
+};
+
 function PortableTextAccordion(content) {
   return (
-    <Accordion type='multiple'>
+    <Accordion type='single' defaultValue='item-0'>
       {content.map((obj, index) => (
         <AccordionItem key={index} value={`item-${index}`}>
-          <div className='font-light text-[#283619] text-xl leading-loose'>
+          <div className='font-light text-[#283619] text-xl leading-loose '>
             <AccordionTrigger>{obj.title}</AccordionTrigger>
           </div>
           <AccordionContent>
             <div className='font-light text-[#283619] text-xl leading-loose'>
-              <PortableText value={obj.description} />
+              <PortableText components={components} value={obj.description} />
             </div>
           </AccordionContent>
         </AccordionItem>
@@ -62,6 +79,11 @@ export default function Product({ product }: { product: SanityDocument }) {
                 {title}
               </h1>
             ) : null}
+            {description ? (
+              <p className='text-[#0B0014] text-left font-normal leading-loose poppins text-xl'>
+                {description}
+              </p>
+            ) : null}
             {accordioninfo ? (
               <div>{PortableTextAccordion(accordioninfo)}</div>
             ) : null}
@@ -73,14 +95,14 @@ export default function Product({ product }: { product: SanityDocument }) {
           >
             {image ? (
               <Image
-                className='float-center m-0 w-1/3 mr-4 rounded-lg pb-5'
+                className='float-center m-0 w-100 rounded-lg mb-5'
                 src={builder.image(image).quality(80).url()}
-                width={300}
-                height={300}
+                width={1300}
+                height={1300}
                 alt={image.alt || ''}
               />
             ) : null}
-            {title ? (
+            {/* {title ? (
               <h1 className='italic text-[#0B0014] font-light text-4xl poppins mb-8'>
                 {title}
               </h1>
@@ -89,7 +111,7 @@ export default function Product({ product }: { product: SanityDocument }) {
               <p className='text-[#0B0014] text-center font-normal leading-loose poppins text-xl'>
                 {description}
               </p>
-            ) : null}
+            ) : null} */}
           </div>
         </div>
       </div>

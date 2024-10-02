a2d1d91729a51899fa5bc6ac8f21fb67d293a71c
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2024-10-01 22:38:12 -0400

    Create query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -379,10 +379,9 @@ export const CONTACT_DETAILS_QUERY = groq`*[_type == "contactDetails"][0]{
   href
 }`;
 
-export const POPUP_CONTENT_QUERY = groq`[_type == "banner"][0]{
-    message,
+export const POPUP_CONTENT_QUERY = groq`[_type == "popup"][0]{
+    content,
     isVisible,
-    storeHours
   }`;
 
 export const CONTACT_PAGE_QUERY = groq`{

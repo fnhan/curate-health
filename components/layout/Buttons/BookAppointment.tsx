d666d1de336f88c4c1b2d0a11ff6f175f11f813b
Ian hu <h878688736@gmail.com>
2024-10-21 11:18:58 -0700

    hero section main page

--- components/layout/Buttons/BookAppointment.tsx
+++ components/layout/Buttons/BookAppointment.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function BookAppointment() {
   return (
     <Link href={'/booking'}>
-      <Button className='bg-transparent text-xl text-white border border-white hover:text-white hover:bg-white hover:text-black rounded-none duration-300 transition-all px-8 py-8'>
+      <Button className='bg-transparent text-xl text-white border border-white hover:text-white hover:bg-white hover:text-primary rounded-none duration-300 transition-all px-8 py-8'>
         Book Appointment
       </Button>
     </Link>

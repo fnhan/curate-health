010928ff10e93fbede3712e917c90cba7fc77ce0
Sean Gray <seangray.dev@gmail.com>
2024-12-08 09:44:36 -0500

    feat: refactor to use alternating-sections comp

--- app/about/our-story/page.tsx
+++ app/about/our-story/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-import { PortableText } from "@portabletext/react";
-
+import { AlternatingSections } from "@/components/shared/alternating-sections";
 import { Button } from "@/components/ui/button";
 import { OUR_STORY_PAGE_QUERYResult } from "@/sanity.types";
 import { OUR_STORY_PAGE_QUERY } from "@/sanity/lib/queries";
@@ -55,42 +54,7 @@ export default async function OurStoryPage() {
           </p>
         </div>
       </section>
-      <section className="-z-20 flex flex-col gap-20 bg-white py-14 text-primary md:gap-28">
-        {additionalSections?.map((section, index) => (
-          <div
-            key={section.sectionTitle}
-            className="flex flex-col gap-16 md:container md:grid md:grid-cols-2 md:gap-16"
-          >
-            <div className={`relative ${index % 2 === 1 ? "md:order-2" : ""}`}>
-              <Image
-                loading="lazy"
-                src={section.sectionImage?.image || ""}
-                alt={section.sectionImage?.alt || ""}
-                width={704}
-                height={556}
-                className={`h-[360px] w-full object-cover md:h-[556px] ${
-                  index % 2 === 0
-                    ? "pr-4 sm:pr-8 md:pr-0"
-                    : "pl-4 sm:pl-8 md:pl-0"
-                }`}
-              />
-              <div
-                className={`absolute -top-7 -z-10 h-[387px] bg-secondary md:h-[487px] ${
-                  index % 2 === 0
-                    ? "left-0 w-[calc(100%-2rem)] sm:w-[calc(100%-4rem)]"
-                    : "right-0 w-[calc(100%-2rem)] sm:w-[calc(100%-4rem)]"
-                }`}
-              />
-            </div>
-            <div className="container flex flex-col gap-4 md:max-w-none md:px-0">
-              <h2 className="text-2xl font-light">{section.sectionTitle}</h2>
-              <div className="prose max-w-[80ch]">
-                <PortableText value={section.sectionParagraph!} />
-              </div>
-            </div>
-          </div>
-        ))}
-      </section>
+      <AlternatingSections sections={additionalSections!} />
       <section className={`relative h-full md:h-[calc(100vh-100px)]`}>
         <Image
           loading="lazy"

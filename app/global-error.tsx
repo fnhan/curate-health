a9a52848a1074d7757806235130038fef0d77cd4
Sean Gray <seangray.dev@gmail.com>
2025-01-05 15:20:28 -0500

    feat: update global-error.tsx

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import Link from "next/link";
+
+import { HomeIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 export default function GlobalError({
   error,
   reset,
@@ -9,9 +15,26 @@ export default function GlobalError({
 }) {
   return (
     <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+      <body className="flex min-h-screen flex-col items-center justify-center gap-4 bg-white">
+        <h2 className="text-2xl font-bold text-black">
+          We're sorry, but something went wrong!
+        </h2>
+        <Button
+          variant={"destructive"}
+          className="rounded-none"
+          onClick={() => reset()}
+        >
+          Try again
+        </Button>
+        <Button
+          asChild
+          className="flex items-center gap-2 rounded-none hover:underline"
+        >
+          <Link href="/">
+            <HomeIcon size={16} />
+            <span className="">Return to Home page</span>
+          </Link>
+        </Button>
       </body>
     </html>
   );
